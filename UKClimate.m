c3a2b5d17de04343ad06c1747112f6acb5ab3a5a
Jordi BP <98339207+Jordi-London@users.noreply.github.com>
2023-02-07 08:38:24 +0000

    Update UKClimate.m
    Matlab code to create spatial maps of climate regions in the UK and also to identify grid within each catchment which is the most "climate representative"

--- UKClimate.m
+++ UKClimate.m
@@ -1,15 +1,27 @@
-%%Reading in the data:
-%info on variables:
-%https://www.metoffice.gov.uk/research/climate/maps-and-data/data/haduk-grid/datasets 
-%Data also available here:
-%"C:\Users\jordi\OneDrive - Imperial College London\Documents - Research
+%%%%%%%%%
+%%%%%%%%%
+%The below code has the following missions:
+
+%Part 1)Create maps which illustrate the spatial variability of UK climate for different climate variables.
+
+%Part 2)Reveal which 1km^(2) grid within each UK basin is the most "climate
+%representative".
+
+%%%%%%%%%%%%%
+% Climate Data can be downlaoded at: https://www.metoffice.gov.uk/research/climate/maps-and-data/data/haduk-grid/datasets
+% Basins Shapefile can be downloaded at: https://ukclimateprojections-ui.metoffice.gov.uk/help/spatial_files
+
+%For those in my research group data is also available here: "C:\Users\jordi\OneDrive - Imperial College London\Documents - Research
 %Data - CI\Jordi\HADUK\1km"
-%%%%%
-%%%%%
+
+%%%%%%%%%%
+%%%%%%%%%%
+%%
 %Start here:
+%Part 1:
 clear
-%Step 1: Import the variables we are interested in
-%here we are workign with monthly averaged UK climate data (30 years)
+%Step 1: Import the climate variables we are interested in. The below are
+%monthly averaged UK climate data over 30years for each 1km2 grid cell. 
 filePr= "C:\Users\jordi\OneDrive - Imperial College London\Desktop\Data\HADUK\1km\rainfall_hadukgrid_uk_1km_mon-30y_199101-202012.nc";
 fileTas ="C:\Users\jordi\OneDrive - Imperial College London\Desktop\Data\HADUK\1km\tas_hadukgrid_uk_1km_mon-30y_199101-202012.nc";
 filesun = "C:\Users\jordi\OneDrive - Imperial College London\Desktop\Data\HADUK\1km\sun_hadukgrid_uk_1km_mon-30y_199101-202012.nc";
@@ -17,27 +29,25 @@ filewind ="C:\Users\jordi\OneDrive - Imperial College London\Desktop\Data\HADUK\
 filehurs = "C:\Users\jordi\OneDrive - Imperial College London\Desktop\Data\HADUK\1km\hurs_hadukgrid_uk_1km_mon-30y_199101-202012.nc";
 filepsl = "C:\Users\jordi\OneDrive - Imperial College London\Desktop\Data\HADUK\1km\psl_hadukgrid_uk_1km_mon-30y_199101-202012.nc";
 
-%Step 2: Have a look at what the variables show
-ncinfo(filePr) %info on the file structure 
-ncdisp(filepsl) %info on the variables contained in the file  
+%Step 2:Have a look at what the variables are
+%ncinfo(filePr) %info on the file structure 
+%ncdisp(filepsl) %info on the variables contained in the file  
 
 %Step 3: create variables
 lat = ncread(filePr,'latitude');
-lon =ncread(filePr,'longitude');%this should be the same for all the files so we only do it once.
-Pr = ncread(filePr,'rainfall');
-tas =ncread(fileTas,'tas');
-sun = ncread(filesun,'sun');
-wind = ncread(filewind,'sfcWind');
-hurs= ncread(filehurs,'hurs');
-psl= ncread(filepsl,'psl');
+lon =ncread(filePr,'longitude');%the lat lon is the same for all the files.
+pr = ncread(filePr,'rainfall'); % Climate Variable 1 - Precipitation
+tas =ncread(fileTas,'tas'); %Climate Variable 2 - temperature 
+sun = ncread(filesun,'sun');% Climate Variable 3 - sunlight
+wind = ncread(filewind,'sfcWind');% Climate Variable 4 - wind
+hurs= ncread(filehurs,'hurs');% Climate Variable 5 - humidity 
+psl= ncread(filepsl,'psl');% Climate Variable 6 - pressure
 
-%Quick note: We have a 3D array (e.g.Pr is 900x150x12) so we need to split the third
-%dimension(12) which corresponds to each month, e.g 1 here would be Jan, 2
-%would be feb...
-%%%%%%%%%%%%%
-%%%%%%%%%%%%%
-%Step 4: Create Plots:
-%%
+%step 4: Set coordinate refences
+proj = projcrs(27700); %def proj - osg 36 - what UKCP18 uses. 
+[xg, yg] = projfwd(proj, lat, lon); %change lat and long to proj x and y
+
+% %Step 4.5: Creating plots:
 
 Variable = sun; %Insert desired variable.Options:'Pr' 'tas' 'sun' 'wind' 'hurs' 'psl'
 
@@ -60,331 +70,125 @@ for i = 1:12
     ylabel('longitude')
 end
 %loop End
+
+%%%%%%%
+%%%%%%%
+%%%%%%%
+%Part 2:
+%Step 5: Loading basins shapefile
+basins= "C:\Users\jordi\OneDrive - Imperial College London\Desktop\CurrentWork\Paper 2\QGIS\ukcp18-uk-land-river-hires.shp";
+
+%Read = readgeotable("C:\Users\jordi\OneDrive - Imperial College London\Desktop\CurrentWork\Paper 2\QGIS\ukcp18-uk-land-river-hires.shp"); %table of what data is in the shapefile
+basins = shaperead (basins); %loading the shapefile
+
+%basinsr = "C:\Users\jordi\OneDrive - Imperial College London\Desktop\CurrentWork\Paper 2\QGIS\UKSHP27700.shp";
+%basinsr = shaperead (basinsr); %loading the shapefile
+
+%mapshow(basins1) %have a look at shapefile
+
+%Step 6: Get climate variables for each basin. 
+
+    for i=1:numel(basins) %so for 1:23
+        X = basins(i).X;
+        Y = basins(i).Y; %lat and lon for basin i
+        in = inpolygon(xg, yg, X, Y); % mask for basin i.
+        %masked = in.*(pr(:,:,1));
+        %prbasins(:,:,i) = masked; %we get 900x1450x23
+            for m = 1:12 % for every month
+                monthlypr = in.*(pr(:,:,m)); %1
+                monthlypr(monthlypr==0)=NaN;
+                monthlytas = in.*(tas(:,:,m));%2
+                monthlytas(monthlytas==0)=NaN;
+                monthlywind = in.*(wind(:,:,m));%3 - works
+                monthlywind(monthlywind==0)=NaN;
+                monthlyhurs = in.*(hurs(:,:,m));%4
+                monthlyhurs(monthlyhurs==0)=NaN;
+                monthlypsl = in.*(psl(:,:,m));%5
+                monthlypsl(monthlypsl==0)=NaN;
+                monthlysun = in.*(sun(:,:,m));%6
+                monthlysun(monthlysun==0)=NaN;
+                output(:,:,i,m,1) = monthlypr;
+                output(:,:,i,m,2) = monthlytas;
+                output(:,:,i,m,3) = monthlywind;
+                output(:,:,i,m,4) = monthlyhurs;
+                output(:,:,i,m,5) = monthlypsl;
+                output(:,:,i,m,6) = monthlysun;
+            end
+    end 
+    
+    clearvars -except basins output 
+
+ %%  
+% Step 7: Get lat lon of most climate representative point.
+T = struct2table(basins);
+points = table();
+points.region= T.geo_region;
+points.lat = zeros(size(T.geo_region));
+points.lon = zeros(size(T.geo_region));
+
+E = zeros(size(output)); % 900x1450x23x12 - only for pr
+for b =1:23
+
+    for m = 1:12 % if b=1 output should be lat long for basin 1 %1:5 are climate variables.
+        E(:,:,b,m) = abs(output(:,:,b,m,1)-mean(output(:,:,b,m,1),'all','omitnan'))/abs(mean(output(:,:,b,m,1),'all','omitnan')) + ...
+        abs(output(:,:,b,m,2)-mean(output(:,:,b,m,2),'all','omitnan'))/abs(mean(output(:,:,b,m,2),'all','omitnan')) + ...
+        abs(output(:,:,b,m,3)-mean(output(:,:,b,m,3),'all','omitnan'))/abs(mean(output(:,:,b,m,3),'all','omitnan')) + ...
+        abs(output(:,:,b,m,4)-mean(output(:,:,b,m,4),'all','omitnan'))/abs(mean(output(:,:,b,m,4),'all','omitnan')) + ...
+        abs(output(:,:,b,m,5)-mean(output(:,:,b,m,5),'all','omitnan'))/abs(mean(output(:,:,b,m,5),'all','omitnan')) + ...
+        abs(output(:,:,b,m,6)-mean(output(:,:,b,m,6),'all','omitnan'))/abs(mean(output(:,:,b,m,6),'all','omitnan'));
+    end
+
+%E_all = sum(E, 4); %supposed to get E for each grid here. 
+E_all = E(:,:,b,1)+E(:,:,b,2)+E(:,:,b,3)+E(:,:,b,4)+E(:,:,b,5)+E(:,:,b,6); %Overall E for each grid here. 
+
+[i, j] = find(E_all == min(E_all, [], 'all', 'omitnan'));
+lon = ncread("C:\Users\jordi\OneDrive - Imperial College London\Desktop\Data\HADUK\1km\psl_hadukgrid_uk_1km_mon-30y_199101-202012.nc", 'longitude');
+lat = ncread("C:\Users\jordi\OneDrive - Imperial College London\Desktop\Data\HADUK\1km\psl_hadukgrid_uk_1km_mon-30y_199101-202012.nc", 'latitude');
+%fprintf('most representative location Lon: %f, Lat: %f \n',lon(i, j),lat(i, j))
+
+points.lat(b) = lat(i, j);
+points.lon(b) = lon(i, j);
+end 
+
+%%%%%
+%%%%%
+%Step 8: Save results.
+save('UKclimatebasins','points','points');
+
+save('myFile.mat', 'output', '-v7.3') % if we want to save a file which is over 2GB
+
+proj = projcrs(27700); %def proj - osg 36 - what UKCP18 uses. 
+for i=1:23
+    [lat, lon] = projinv(proj, basins(i).X, basins(i).Y); %change lat and long to proj x and y
+    geoplot(lat,lon)
+    hold on
+end 
+hold on 
+geoscatter(points.lat,points.lon, "MarkerEdgeColor","k")
+geolimits([48 60],[-5 5])
+
+%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%
 %%
-%%%%%%%%%%%%%%%
-%%%%%%%%%%%%%%%
-%% If we want to use monthly colorbar axis scales or histogram bins:
-sgtitle("UK Monthly Averaged Rainfall (mm) over 30yr")
-%Jan
-subplot(4, 3, 1); %we create a 4*3 array and then plot on the first box.
-geoshow(lat, lon, sun(:,:,1), 'displaytype', 'surface');
-colorbar('vert','FontSize',12);
-%caxis([0 200]); %we can play with the color bar scale here... - perhaps bins would be more appropriate...
-title('Jan Pr (mm)');
-xlim([-9 2])
-ylim([49 60])
-xlabel('latitude')
-ylabel('longitude')
-%Feb
-subplot(4, 3, 2); %we create a 4*3 array and then plot on the first box.
-geoshow(lat, lon, Pr(:,:,2), 'displaytype', 'surface');
-colorbar('vert','FontSize',12);
-caxis([0 200]);
-title('Feb');
-xlim([-9 2])
-ylim([49 60])
-xlabel('latitude')
-ylabel('longitude')
-%March
-subplot(4, 3, 3);
-geoshow(lat, lon, Pr(:,:,3), 'displaytype', 'surface');
-colorbar('vert','FontSize',12);
-caxis([0 200]);
-title('Mar Pr (mm)');
-xlim([-9 2])
-ylim([49 60])
-xlabel('latitude')
-ylabel('longitude')
-
-%April
-subplot(4, 3, 4);
-geoshow(lat, lon, Pr(:,:,4), 'displaytype', 'surface');
-colorbar('vert','FontSize',12);
-caxis([0 200]);
-title('Apr Pr (mm)');
-xlim([-9 2])
-ylim([49 60])
-xlabel('latitude')
-ylabel('longitude')
-%May
-subplot(4, 3, 5);
-geoshow(lat, lon, Pr(:,:,5), 'displaytype', 'surface');
-colorbar('vert','FontSize',12);
-caxis([0 200]);
-title('May Pr (mm)');
-xlim([-9 2])
-ylim([49 60])
-xlabel('latitude')
-ylabel('longitude')
-
-%June
-subplot(4, 3, 6);
-geoshow(lat, lon, Pr(:,:,6), 'displaytype', 'surface');
-colorbar('vert','FontSize',12);
-caxis([0 200]);
-title('Jun Pr (mm)');
-xlim([-9 2])
-ylim([49 60])
-xlabel('latitude')
-ylabel('longitude')
-%July
-subplot(4, 3, 7);
-geoshow(lat, lon, Pr(:,:,7), 'displaytype', 'surface');
-colorbar('vert','FontSize',12);
-caxis([0 200]);
-title('Jul Pr (mm)');
-xlim([-9 2])
-ylim([49 60])
-xlabel('latitude')
-ylabel('longitude')
-
-%August 
-subplot(4, 3, 8);
-geoshow(lat, lon, Pr(:,:,8), 'displaytype', 'surface');
-colorbar('vert','FontSize',12);
-caxis([0 200]);
-title('Aug Pr (mm)');
-xlim([-9 2])
-ylim([49 60])
-xlabel('latitude')
-ylabel('longitude')
-
-%September
-subplot(4, 3, 9);
-geoshow(lat, lon, Pr(:,:,9), 'displaytype', 'surface');
-colorbar('vert','FontSize',12);
-caxis([0 200]);
-title('Sept Pr (mm)');
-xlim([-9 2])
-ylim([49 60])
-xlabel('latitude')
-ylabel('longitude')
-
-%October
-subplot(4, 3, 10);
-geoshow(lat, lon, Pr(:,:,10), 'displaytype', 'surface');
-colorbar('vert','FontSize',12);
-caxis([0 200]);
-title('Oct Pr (mm)');
-xlim([-9 2])
-ylim([49 60])
-xlabel('latitude')
-ylabel('longitude')
-
-%November
-subplot(4, 3, 11);
-geoshow(lat, lon, Pr(:,:,11), 'displaytype', 'surface');
-colorbar('vert','FontSize',12);
-caxis([0 200]);
-title('Nov Pr (mm)');
-xlim([-9 2])
-ylim([49 60])
-xlabel('latitude')
-ylabel('longitude')
-
-%December
-subplot(4, 3, 12);
-geoshow(lat, lon, Pr(:,:,12), 'displaytype', 'surface');
-colorbar('vert','FontSize',12);
-caxis([0 200]);
-title('Dec Pr (mm)');
-xlim([-9 2])
-ylim([49 60])
-xlabel('latitude')
-ylabel('longitude')
-
-%% %% Monthly Temperature
-%Jan
-subplot(4, 3, 1);
-mymap=pcolor(latitude,longitude,Jantas)
-mymap.EdgeAlpha = 0;
-colorbar
-xlabel('latitude')
-ylabel('longitude')
-title('Jan Avg Temp (C) 1960-2020')
-%caxis([0 250])
-xlim([0 700000])
-ylim([0 1300000])
-
-%Feb
-subplot(4, 3, 2);
-mymap=pcolor(latitude,longitude,Febtas)
-mymap.EdgeAlpha = 0;
-colorbar
-xlabel('latitude')
-ylabel('longitude')
-title('Feb')
-%caxis([0 250]) %to set the colour gradient scale 
-xlim([0 700000]) %setting the lower and upper limits of the graph axis
-ylim([0 1300000])
-
-%March
-subplot(4, 3, 3);
-mymap=pcolor(latitude,longitude,Martas)
-mymap.EdgeAlpha = 0;
-colorbar
-xlabel('latitude')
-ylabel('longitude')
-title('Mar')
-%caxis([0 250])
-xlim([0 700000])
-ylim([0 1300000])
-
-%April
-subplot(4, 3, 4);
-mymap=pcolor(latitude,longitude,Aprtas)
-mymap.EdgeAlpha = 0;
-colorbar
-xlabel('latitude')
-ylabel('longitude')
-title('Apr')
-%caxis([0 250])
-xlim([0 700000])
-ylim([0 1300000])
-%May
-subplot(4, 3, 5);
-mymap=pcolor(latitude,longitude,Maytas)
-mymap.EdgeAlpha = 0;
-colorbar
-xlabel('latitude')
-ylabel('longitude')
-title('May')
-%caxis([0 250])
-xlim([0 700000])
-ylim([0 1300000])
-%June
-subplot(4, 3, 6);
-mymap=pcolor(latitude,longitude,Juntas)
-mymap.EdgeAlpha = 0;
-colorbar
-xlabel('latitude')
-ylabel('longitude')
-title('Jun')
-%caxis([0 250])
-xlim([0 700000])
-ylim([0 1300000])
-
-%July
-subplot(4, 3, 7);
-mymap=pcolor(latitude,longitude,Jultas)
-mymap.EdgeAlpha = 0;
-colorbar
-xlabel('latitude')
-ylabel('longitude')
-title('Jul')
-%caxis([0 250])
-xlim([0 700000])
-ylim([0 1300000])
-
-%August 
-subplot(4, 3, 8);
-mymap=pcolor(latitude,longitude,Augtas)
-mymap.EdgeAlpha = 0;
-colorbar
-xlabel('latitude')
-ylabel('longitude')
-title('Aug')
-%caxis([0 250])
-xlim([0 700000])
-ylim([0 1300000])
-
-%September
-subplot(4, 3, 9);
-mymap=pcolor(latitude,longitude,Septas)
-mymap.EdgeAlpha = 0;
-colorbar
-xlabel('latitude')
-ylabel('longitude')
-title('Sep')
-%caxis([0 250])
-xlim([0 700000])
-ylim([0 1300000])
-
-%October
-subplot(4, 3, 10);
-mymap=pcolor(latitude,longitude,Octtas)
-mymap.EdgeAlpha = 0;
-colorbar
-xlabel('latitude')
-ylabel('longitude')
-title('Oct')
-%caxis([0 250])
-xlim([0 700000])
-ylim([0 1300000])
-%November
-subplot(4, 3, 11);
-mymap=pcolor(latitude,longitude,Novtas)
-mymap.EdgeAlpha = 0;
-colorbar
-xlabel('latitude')
-ylabel('longitude')
-title('Nov')
-%caxis([0 250])
-xlim([0 700000])
-ylim([0 1300000])
-
-%December
-subplot(4, 3, 12);
-mymap=pcolor(latitude,longitude,Dectas)
-mymap.EdgeAlpha = 0;
-colorbar
-xlabel('latitude')
-ylabel('longitude')
-title('Dec')
-%caxis([0 250])
-xlim([0 700000])
-ylim([0 1300000])
-
 
 %% Now time for a K-means Cluster ! 
-
-clear
-
-%Example below:
-cd 'C:\Users\jordi\OneDrive - Imperial College London\Desktop\Testing\23rdJan'
-
-
-M = {'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'};
-
-for m = 1:12
-    A = arcgridread([M{m}, '.txt']);
-    T(:,:,m) = A;
-    X(:,m) = A(:);
-end
-
-[C,I] = kmeans(X, 7);
-
-imagesc(reshape(C,290,180));
-
-%% seeing how the above works. 
-
-TD = readmatrix("C:\Users\jordi\Downloads\Jan.txt");
-
-TD  = arcgridread("Jan.txt");
-
-%testing the above with m=1
-%first loop
-m=1;
-
-A = arcgridread([M{m}, '.txt']);
-T(:,:,m) = A;
-X(:,m) = A(:);
-%second loop
-m=2;
-
-A = arcgridread([M{m}, '.txt']);
-T(:,:,m) = A;
-X(:,m) = A(:);
-
-%% Next steps:
-
-%1  %Separate the above climate variables into the 23 river basin regions of
-%the UK - I have this shapefile somewhere. 
-
-%2 : Use some sort of weighting ? to find pixel areas which are most
-%representative within each river basin ? can then run this as a loop. 
-
-
-
-
-
+% 
+% clear
+% 
+% %Example below:
+% cd 'C:\Users\jordi\OneDrive - Imperial College London\Desktop\Testing\23rdJan'
+% 
+% 
+% M = {'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'};
+% 
+% for m = 1:12
+%     A = arcgridread([M{m}, '.txt']);
+%     T(:,:,m) = A;
+%     X(:,m) = A(:);
+% end
+% 
+% [C,I] = kmeans(X, 7);
+% 
+% imagesc(reshape(C,290,180));
